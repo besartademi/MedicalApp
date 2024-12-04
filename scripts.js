b1e1cccf2c775aca479832bf8c66458142eaaf87
Besart Ademi <besartademi@users.noreply.github.com>
2024-12-04 23:27:06 +0100

    minor fixes

--- scripts.js
+++ scripts.js
@@ -1,2 +1,4 @@
 // Default date set to today's date
-document.getElementById('date-select').value = new Date().toISOString().split('T')[0];
+document.getElementById("date-select").value = new Date()
+    .toISOString()
+    .split("T")[0];
